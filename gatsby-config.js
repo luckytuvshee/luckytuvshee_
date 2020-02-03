b8d8cafcf7fbb68e069fafec97ef5d4a5f62d38e
Tuvshinsanaa Tuul <luckytuvshee@yahoo.com>
2020-02-03 23:55:49 +0800

    added deploy(gh-pages) script

--- gatsby-config.js
+++ gatsby-config.js
@@ -44,6 +44,7 @@ module.exports = {
         icon: `src/images/luckytuvshee_logo.jpeg`, // This path is relative to the root of the site.
       },
     },
+
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
